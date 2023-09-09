18df916b6fc64c5d87814d42d9b4a326725ddfca
ayushi srivastava <ayushisrivastavas29@gmail.com>
2023-09-09 10:18:08 +0530

    first commit

---
+++ myVenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ayush\OneDrive\Desktop\onlinexamination\onlinexamination\myVenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
